c1e322cc3f11edb6744c7c0f5cd8a394cb4b31ea
Simon Karman <mail@simonkarman.nl>
2025-07-23 23:13:05 +0200

    Created printable deck page in client at /deck

---
+++ client/src/app/deck/page.tsx
@@ -0,0 +1,64 @@
+import Link from 'next/link';
+import { capitalize } from '@/utils/typography';
+import { getCards } from '@/utils/server';
+import { Card } from 'kindred-paths';
+import { CardRender } from '@/components/card-render';
+
+const n = (count: number) => Array.from({ length: count }, (_, i) => i + 1);
+const getCount = (card: Card) => typeof card.tags?.['count'] === 'number' ? card.tags['count'] : 0;
+
+export default async function DeckOverview() {
+  const deckName = "entice";
+  const _cards = await getCards();
+
+  const cards = _cards
+    .filter(c => c.tags?.["deleted"] !== true && c.tags?.["deck"] === deckName)
+    .toSorted((a, b) => a.collectorNumber - b.collectorNumber)
+    .map(c => new Card(c));
+
+  const totalCount = cards.reduce((acc, card) => acc + getCount(card), 0);
+
+  const manaValueDistribution = cards.reduce((acc, card) => {
+    const manaCost = card.manaValue();
+    const count = getCount(card);
+    if (manaCost === 0) {
+      return acc;
+    }
+
+    if (manaCost in acc) {
+      acc[manaCost] += count;
+    } else {
+      acc[manaCost] = count;
+    }
+    return acc;
+  }, {} as { [manaValue: number]: number });
+
+  return <>
+    <p className="px-2">
+      <Link href={'/'} className="underline text-blue-600">Go back</Link>
+    </p>
+    <h2 className="font-lg font-bold">{capitalize(deckName)}</h2>
+    <p className="text-gray-500 text-sm italic">This is the deck overview for {deckName}.</p>
+    <ul>
+      {cards.map(card => (
+        <li key={card.id} className="border-b border-gray-200 py-2">
+          {getCount(card)}x{' '}
+          <Link href={`/card/${card.id}`} className="text-blue-600 hover:underline">
+            #{card.collectorNumber} {card.name}: {card.renderTypeLine()}
+          </Link>
+        </li>
+      ))}
+      {cards.length === 0 && (
+        <li className="text-gray-500">No cards in this deck.</li>
+      )}
+    </ul>
+    <p>
+      <strong>Total cards:</strong> {totalCount}<br/>
+      <strong>Mana Value Distribution</strong>
+      <pre>{JSON.stringify(manaValueDistribution, undefined, 2)}</pre>
+    </p>
+    <div className="grid grid-cols-3">
+      {cards.map(card => n(getCount(card)).map(i => <CardRender key={card.id + i} serializedCard={card.toJson()} />))}
+    </div>
+  </>;
+};
