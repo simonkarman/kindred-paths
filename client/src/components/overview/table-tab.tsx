d4b90098cab4bc5b3b30d4d8d048fbb92742875b
Simon Karman <mail@simonkarman.nl>
2025-10-29 23:46:58 +0100

    Imrpoved searching for dfc and ensure table tab highlights dfc correctly on hover

--- client/src/components/overview/table-tab.tsx
+++ client/src/components/overview/table-tab.tsx
@@ -175,108 +175,106 @@ export const TableTab = (props: {
             {!onSelect && <th className="px-3 py-2 text-center text-xs font-medium text-slate-600">Actions</th>}
           </tr>
           </thead>
-          <tbody className="divide-y divide-slate-100">
-          {cards.flatMap((card) => card.faces).map(face => {
-            const faceIndex = face.card.faces.indexOf(face);
-            const isFirstFaceOfDualCard = face.card.faces.length === 2 && faceIndex === 0;
-            const isSecondFaceOfDualCard = face.card.faces.length === 2 && faceIndex === 1;
-            const doubleRowSpan = isFirstFaceOfDualCard ? 2 : 1;
-            const showDoubleRows = !isSecondFaceOfDualCard;
-            const borderColor = 'border-slate-300';
-            const defaultPaddingY = `border-y py-2 ${borderColor}`;
-            const paddingY = face.card.faces.length === 1
-              ? defaultPaddingY
-              : (isFirstFaceOfDualCard ? `border-t pt-4 pb-0.5 ${borderColor}` : `pt-0 pb-4 border-b ${borderColor}`);
-            const doubleRowPaddingY = defaultPaddingY;
+          {cards.map(card => <tbody
+            key={card.id}
+            className="hover:bg-slate-50 transition-colors border-y border-slate-300"
+          >
+            {card.faces.map((face, faceIndex) => {
+              const isFirstFaceOfDualCard = face.card.faces.length === 2 && faceIndex === 0;
+              const isSecondFaceOfDualCard = face.card.faces.length === 2 && faceIndex === 1;
+              const doubleRowSpan = isFirstFaceOfDualCard ? 2 : 1;
+              const showDoubleRows = !isSecondFaceOfDualCard;
+              const defaultPaddingY = `py-2`;
+              const paddingY = face.card.faces.length === 1
+                ? defaultPaddingY
+                : (isFirstFaceOfDualCard ? `pt-4 pb-1` : `pt-1 pb-4`);
+              const doubleRowPaddingY = defaultPaddingY;
+              const creatableTokenNames = face.getCreatableTokenNames();
 
-            const cardDisplayName = face.name + (isFirstFaceOfDualCard ? ' //' : '');
-            const creatableTokenNames = face.getCreatableTokenNames();
-
-            return <tr
-              key={`${face.card.id}-${faceIndex}`}
-              className="hover:bg-slate-50 transition-colors"
-            >
-              <td className={`px-3 ${paddingY} text-right whitespace-nowrap`}>
-                <ManaCost cost={face.renderManaCost()} />
-              </td>
-              {showDoubleRows && <td rowSpan={doubleRowSpan} className={`px-3 ${doubleRowPaddingY} text-center text-slate-600 text-xs`}>
-                {face.card.tags?.[`deck/${deckName}`] ? face.card.tags?.[`deck/${deckName}`] + 'x' : '-'}
-              </td>}
-              {showDoubleRows && <td rowSpan={doubleRowSpan} className={`px-3 ${doubleRowPaddingY} text-right text-slate-600 text-xs whitespace-nowrap`}>
-                {(face.card.tags?.['set'] ? face.card.tags?.['set'] + '/' : '') + face.card.collectorNumber.toString()}
-              </td>}
-              <td className={`px-3 ${paddingY} font-semibold`}>
-                {onSelect !== undefined ? (
-                  <button
-                    className="hover:text-blue-600 active:text-blue-700 transition-colors"
-                    onClick={() => onSelect(face.card.toJson(), faceIndex)}
-                  >
-                    {cardDisplayName}
-                  </button>
-                ) : (
-                  <Link
-                    className="hover:text-blue-600 active:text-blue-700 transition-colors"
-                    href={`/card/${face.card.id}${faceIndex !== 0 ? `?faceIndex=${faceIndex}` : ''}`}
-                  >
-                    {cardDisplayName}
-                  </Link>
-                )}
-              </td>
-              {showDoubleRows && <td rowSpan={doubleRowSpan} className={`px-3 ${doubleRowPaddingY}`}>
-                <RarityText rarity={face.card.rarity} />
-              </td>}
-              <td className={`px-3 ${paddingY} text-slate-700`}>
-                {face.renderTypeLine()}</td>
-              <td className={`px-3 ${paddingY} text-center text-slate-700`}>
-                {face.pt ? `${face.pt.power}/${face.pt.toughness}` : ''}
-              </td>
-              <td className={`px-3 ${paddingY} text-center`}>
-                <FontAwesomeIcon className={face.art ? 'text-slate-500' : 'text-slate-200'} icon={faImage} />
-              </td>
-              <td className={`group relative px-3 ${paddingY} text-center`}>
-                <FontAwesomeIcon
-                  className={creatableTokenNames.length > 0 ? 'text-slate-500' : 'text-slate-200'}
-                  icon={faShieldCat}
-                />
-                {creatableTokenNames.length > 0 && <div
-                  className="hidden group-hover:flex absolute z-100 -top-5 -right-1 gap-2"
-                >
-                  {creatableTokenNames.map(name => <div
-                    key={name}
-                    className="bg-blue-500 text-white text-xs rounded-md px-2 py-1 mt-1 whitespace-nowrap shadow-lg"
-                  >{name}</div>)}
-                </div>}
-              </td>
-              {showDoubleRows && !onSelect && (
-                <td rowSpan={doubleRowSpan} className={`px-3 ${doubleRowPaddingY}`}>
-                  <div className="flex gap-2 justify-center">
-                    <Link
-                      className="text-slate-500 hover:text-blue-600 transition-colors"
-                      href={`/edit/${face.card.id}?t=/`}
-                      title="Edit"
-                    >
-                      <FontAwesomeIcon icon={faPenToSquare} />
-                    </Link>
-                    <Link
-                      className="text-slate-500 hover:text-blue-600 transition-colors"
-                      href={`/clone/${face.card.id}?t=/`}
-                      title="Clone"
-                    >
-                      <FontAwesomeIcon icon={faClone} />
-                    </Link>
-                    <button
-                      className="text-slate-500 hover:text-red-600 transition-colors"
-                      onClick={() => del(face.card.id)}
-                      title="Delete"
-                    >
-                      <FontAwesomeIcon icon={faTrashCan} />
-                    </button>
-                  </div>
+              return <tr
+                key={faceIndex}
+              >
+                <td className={`px-3 ${paddingY} text-right whitespace-nowrap`}>
+                  <ManaCost cost={face.renderManaCost()} />
                 </td>
-              )}
-            </tr>;
-          })}
-          </tbody>
+                {showDoubleRows && <td rowSpan={doubleRowSpan} className={`px-3 ${doubleRowPaddingY} text-center text-slate-600 text-xs`}>
+                  {face.card.tags?.[`deck/${deckName}`] ? face.card.tags?.[`deck/${deckName}`] + 'x' : '-'}
+                </td>}
+                {showDoubleRows && <td rowSpan={doubleRowSpan} className={`px-3 ${doubleRowPaddingY} text-right text-slate-600 text-xs whitespace-nowrap`}>
+                  {(face.card.tags?.['set'] ? face.card.tags?.['set'] + '/' : '') + face.card.collectorNumber.toString()}
+                </td>}
+                <td className={`px-3 ${paddingY} font-semibold`}>
+                  {onSelect !== undefined ? (
+                    <button
+                      className="hover:text-blue-600 active:text-blue-700 transition-colors"
+                      onClick={() => onSelect(face.card.toJson(), faceIndex)}
+                    >
+                      {face.name}
+                    </button>
+                  ) : (
+                    <Link
+                      className="hover:text-blue-600 active:text-blue-700 transition-colors"
+                      href={`/card/${face.card.id}${faceIndex !== 0 ? `?faceIndex=${faceIndex}` : ''}`}
+                    >
+                      {face.name}
+                    </Link>
+                  )}
+                </td>
+                {showDoubleRows && <td rowSpan={doubleRowSpan} className={`px-3 ${doubleRowPaddingY}`}>
+                  <RarityText rarity={face.card.rarity} />
+                </td>}
+                <td className={`px-3 ${paddingY} text-slate-700`}>
+                  {face.renderTypeLine()}</td>
+                <td className={`px-3 ${paddingY} text-center text-slate-700`}>
+                  {face.pt ? `${face.pt.power}/${face.pt.toughness}` : ''}
+                </td>
+                <td className={`px-3 ${paddingY} text-center`}>
+                  <FontAwesomeIcon className={face.art ? 'text-slate-500' : 'text-slate-200'} icon={faImage} />
+                </td>
+                <td className={`group relative px-3 ${paddingY} text-center`}>
+                  <FontAwesomeIcon
+                    className={creatableTokenNames.length > 0 ? 'text-slate-500' : 'text-slate-200'}
+                    icon={faShieldCat}
+                  />
+                  {creatableTokenNames.length > 0 && <div
+                    className="hidden group-hover:flex absolute z-100 -top-5 -right-1 gap-2"
+                  >
+                    {creatableTokenNames.map(name => <div
+                      key={name}
+                      className="bg-blue-500 text-white text-xs rounded-md px-2 py-1 mt-1 whitespace-nowrap shadow-lg"
+                    >{name}</div>)}
+                  </div>}
+                </td>
+                {showDoubleRows && !onSelect && (
+                  <td rowSpan={doubleRowSpan} className={`px-3 ${doubleRowPaddingY}`}>
+                    <div className="flex gap-2 justify-center">
+                      <Link
+                        className="text-slate-500 hover:text-blue-600 transition-colors"
+                        href={`/edit/${face.card.id}?t=/`}
+                        title="Edit"
+                      >
+                        <FontAwesomeIcon icon={faPenToSquare} />
+                      </Link>
+                      <Link
+                        className="text-slate-500 hover:text-blue-600 transition-colors"
+                        href={`/clone/${face.card.id}?t=/`}
+                        title="Clone"
+                      >
+                        <FontAwesomeIcon icon={faClone} />
+                      </Link>
+                      <button
+                        className="text-slate-500 hover:text-red-600 transition-colors"
+                        onClick={() => del(face.card.id)}
+                        title="Delete"
+                      >
+                        <FontAwesomeIcon icon={faTrashCan} />
+                      </button>
+                    </div>
+                  </td>
+                )}
+              </tr>;
+            })}
+          </tbody>)}
         </table>
       </div>
       {cards.length === 0 && (
--- client/src/utils/use-search.ts
+++ client/src/utils/use-search.ts
@@ -96,138 +96,139 @@ export const filterCardsBasedOnSearch = (cards: SerializedCard[], searchText: st
   const searchTerms = searchText.toLowerCase().split(/\s+/);
 
   return cards.filter(_card => {
-    const card = new Card(_card);
-    const cardFace = card.faces[0]; // TODO: support multi-face cards
-    const checks = [
-      check(['layout', 'l'], layoutNeedle => card.layout.startsWith(layoutNeedle)),
+    return new Card(_card).faces.some(cardFace => {
+      const card = cardFace.card;
+      const checks = [
+        check(['layout', 'l'], layoutNeedle => card.layout.startsWith(layoutNeedle)),
 
-      check(['type', 't'], typeNeedle => [
-          ...(card.isToken ? ['token'] : []),
-          ...cardFace.types,
-          ...(cardFace.subtypes ?? []),
-          ...(cardFace.supertype ? [cardFace.supertype] : []),
-        ].some(t => t.startsWith(typeNeedle))),
+        check(['type', 't'], typeNeedle => [
+            ...(card.isToken ? ['token'] : []),
+            ...cardFace.types,
+            ...(cardFace.subtypes ?? []),
+            ...(cardFace.supertype ? [cardFace.supertype] : []),
+          ].some(t => t.startsWith(typeNeedle))),
 
-      check(['rarity', 'r'], rarityNeedle => rarityNeedle.length === 1
-        ? card.rarity[0] === rarityNeedle[0]
-        : card.rarity === rarityNeedle),
+        check(['rarity', 'r'], rarityNeedle => rarityNeedle.length === 1
+          ? card.rarity[0] === rarityNeedle[0]
+          : card.rarity === rarityNeedle),
 
-      check(['color', 'c'], colorNeedle => {
-        if (colorNeedle === 'colorless' || colorNeedle === 'c') {
-          return cardFace.color().length === 0;
-        }
-        if (colorNeedle === 'multicolor' || colorNeedle === 'm') {
-          return cardFace.color().length > 1;
-        }
-        // Convert single character to full color name
-        const c = wubrg.includes(colorNeedle as CardColorCharacter)
-          ? colorToLong(colorNeedle as CardColorCharacter)
-          : colorNeedle;
-        return cardFace.color().includes(c as CardColor);
-      }),
-
-      check(['manavalue', 'mv'], manaValueNeedle => {
-        return validateNumberRequirement(manaValueNeedle, cardFace.manaValue());
-      }),
-
-      check(['pt'], ptNeedle => {
-        if (ptNeedle === 'yes' || ptNeedle === 'no') {
-          // Match "pt:yes" for cards with power/toughness
-          // Match "pt:no" for cards without power/toughness
-          return ptNeedle === 'yes' ? !!cardFace.pt : !cardFace.pt;
-        }
-        if (!cardFace.pt
-          || !ptNeedle.includes('/')
-          || ptNeedle.indexOf('/') !== ptNeedle.lastIndexOf('/')
-        ) return false;
-        if (ptNeedle.includes('n')) {
-          // Match "n/n" for cards with equal power/toughness
-          if (ptNeedle === 'n/n') {
-            return cardFace.pt.power === cardFace.pt.toughness;
-          // Match "n/n+" for cards with greater toughness than power
-          } else if (ptNeedle === 'n/n+') {
-            return cardFace.pt.toughness > cardFace.pt.power;
-          // Match "n+/n" for cards with greater power than toughness
-          } else if (ptNeedle === 'n+/n') {
-            return cardFace.pt.power > cardFace.pt.toughness;
-          // Match "n/n-" for cards with less toughness than power
-          } else if (ptNeedle === 'n/n-') {
-            return cardFace.pt.toughness < cardFace.pt.power;
-          // Match "n-/n" for cards with less power than toughness
-          } else if (ptNeedle === 'n-/n') {
-            return cardFace.pt.power < cardFace.pt.toughness;
-          // Match "n/n+2" for cards with toughness exactly equal to its than power by 2
-          } else if (ptNeedle.startsWith('n/n+')) {
-            const diff = Number(ptNeedle.slice(4));
-            return !isNaN(diff) && (cardFace.pt.toughness - cardFace.pt.power) === diff;
-          // Match "n+3/n" for cards with power exactly equal to its than toughness by 3
-          } else if (ptNeedle.startsWith('n+') && ptNeedle.endsWith('/n')) {
-            const diff = Number(ptNeedle.slice(2, -2));
-            return !isNaN(diff) && (cardFace.pt.power - cardFace.pt.toughness) === diff;
-          // Match "n/n-3" for cards with toughness exactly equal to its than power minus 3
-          } else if (ptNeedle.startsWith('n/n-')) {
-            const diff = Number(ptNeedle.slice(4));
-            return !isNaN(diff) && (cardFace.pt.power - cardFace.pt.toughness) === diff;
-          // Match "n-3/n" for cards with power exactly equal to its than toughness minus 3
-          } else if (ptNeedle.startsWith('n-') && ptNeedle.endsWith('/n')) {
-            const diff = Number(ptNeedle.slice(2, -2));
-            return !isNaN(diff) && (cardFace.pt.toughness - cardFace.pt.power) === diff;
+        check(['color', 'c'], colorNeedle => {
+          if (colorNeedle === 'colorless' || colorNeedle === 'c') {
+            return cardFace.color().length === 0;
           }
-          // Invalid format
-          return false;
+          if (colorNeedle === 'multicolor' || colorNeedle === 'm') {
+            return cardFace.color().length > 1;
+          }
+          // Convert single character to full color name
+          const c = wubrg.includes(colorNeedle as CardColorCharacter)
+            ? colorToLong(colorNeedle as CardColorCharacter)
+            : colorNeedle;
+          return cardFace.color().includes(c as CardColor);
+        }),
+
+        check(['manavalue', 'mv'], manaValueNeedle => {
+          return validateNumberRequirement(manaValueNeedle, cardFace.manaValue());
+        }),
+
+        check(['pt'], ptNeedle => {
+          if (ptNeedle === 'yes' || ptNeedle === 'no') {
+            // Match "pt:yes" for cards with power/toughness
+            // Match "pt:no" for cards without power/toughness
+            return ptNeedle === 'yes' ? !!cardFace.pt : !cardFace.pt;
+          }
+          if (!cardFace.pt
+            || !ptNeedle.includes('/')
+            || ptNeedle.indexOf('/') !== ptNeedle.lastIndexOf('/')
+          ) return false;
+          if (ptNeedle.includes('n')) {
+            // Match "n/n" for cards with equal power/toughness
+            if (ptNeedle === 'n/n') {
+              return cardFace.pt.power === cardFace.pt.toughness;
+            // Match "n/n+" for cards with greater toughness than power
+            } else if (ptNeedle === 'n/n+') {
+              return cardFace.pt.toughness > cardFace.pt.power;
+            // Match "n+/n" for cards with greater power than toughness
+            } else if (ptNeedle === 'n+/n') {
+              return cardFace.pt.power > cardFace.pt.toughness;
+            // Match "n/n-" for cards with less toughness than power
+            } else if (ptNeedle === 'n/n-') {
+              return cardFace.pt.toughness < cardFace.pt.power;
+            // Match "n-/n" for cards with less power than toughness
+            } else if (ptNeedle === 'n-/n') {
+              return cardFace.pt.power < cardFace.pt.toughness;
+            // Match "n/n+2" for cards with toughness exactly equal to its than power by 2
+            } else if (ptNeedle.startsWith('n/n+')) {
+              const diff = Number(ptNeedle.slice(4));
+              return !isNaN(diff) && (cardFace.pt.toughness - cardFace.pt.power) === diff;
+            // Match "n+3/n" for cards with power exactly equal to its than toughness by 3
+            } else if (ptNeedle.startsWith('n+') && ptNeedle.endsWith('/n')) {
+              const diff = Number(ptNeedle.slice(2, -2));
+              return !isNaN(diff) && (cardFace.pt.power - cardFace.pt.toughness) === diff;
+            // Match "n/n-3" for cards with toughness exactly equal to its than power minus 3
+            } else if (ptNeedle.startsWith('n/n-')) {
+              const diff = Number(ptNeedle.slice(4));
+              return !isNaN(diff) && (cardFace.pt.power - cardFace.pt.toughness) === diff;
+            // Match "n-3/n" for cards with power exactly equal to its than toughness minus 3
+            } else if (ptNeedle.startsWith('n-') && ptNeedle.endsWith('/n')) {
+              const diff = Number(ptNeedle.slice(2, -2));
+              return !isNaN(diff) && (cardFace.pt.toughness - cardFace.pt.power) === diff;
+            }
+            // Invalid format
+            return false;
+          }
+          let toughnessRequirement = '0+', powerRequirement = '0+';
+          if (ptNeedle.startsWith('/')) {
+            // If only toughness is specified (e.g. "/3"), match any power with that toughness
+            toughnessRequirement = ptNeedle.slice(1);
+          } else if (ptNeedle.endsWith('/')) {
+            // If only power is specified (e.g. "3/"), match that power with any toughness
+            powerRequirement = ptNeedle.slice(0, -1);
+          } else {
+            // Otherwise, match exact power/toughness (e.g. "3/4", "2+/3-", etc)
+            [powerRequirement, toughnessRequirement] = ptNeedle.split('/');
+          }
+          return validateNumberRequirement(toughnessRequirement, cardFace.pt.toughness)
+            && validateNumberRequirement(powerRequirement, cardFace.pt.power)
+        }),
+
+        check(['deck', 'd'], deckNameNeedle => {
+          const deckTag = card.getTagAsNumber(`deck/${deckNameNeedle}`);
+          return deckTag !== undefined && deckTag >= 0;
+        }),
+
+        check(['set', 's'], setNameNeedle => {
+          const setTag = card.getTagAsString('set');
+          return setTag !== undefined && setTag.toLowerCase().startsWith(setNameNeedle);
+        }),
+
+        check(['tag'], tagNeedle => {
+          const [key, value] = tagNeedle.split('=').map(s => s.trim());
+          if (value) {
+            // If both key and value are specified (e.g. "tag:foo=bar"), match exact key and value on contains
+            return card.tags !== undefined && Object.entries(card.tags).some(([k, v]) =>
+              k.toLowerCase() === key && v !== undefined && v.toString().toLowerCase().includes(value)
+            );
+          } else {
+            // If only key is specified (e.g. "tag:foo"), match any tag with that key
+            return card.tags !== undefined && Object.keys(card.tags).some(k =>
+              k.toLowerCase() === key
+            );
+          }
+        }),
+      ];
+
+      return searchTerms.every(searchTerm => {
+        // First try all checks
+        for (const check of checks) {
+          const res = check(searchTerm);
+          if (res) return res.result;
         }
-        let toughnessRequirement = '0+', powerRequirement = '0+';
-        if (ptNeedle.startsWith('/')) {
-          // If only toughness is specified (e.g. "/3"), match any power with that toughness
-          toughnessRequirement = ptNeedle.slice(1);
-        } else if (ptNeedle.endsWith('/')) {
-          // If only power is specified (e.g. "3/"), match that power with any toughness
-          powerRequirement = ptNeedle.slice(0, -1);
-        } else {
-          // Otherwise, match exact power/toughness (e.g. "3/4", "2+/3-", etc)
-          [powerRequirement, toughnessRequirement] = ptNeedle.split('/');
-        }
-        return validateNumberRequirement(toughnessRequirement, cardFace.pt.toughness)
-          && validateNumberRequirement(powerRequirement, cardFace.pt.power)
-      }),
+        // If the search term ends with ":", it's an incomplete key, so fail the search
+        if (searchTerm.endsWith(':')) return false;
 
-      check(['deck', 'd'], deckNameNeedle => {
-        const deckTag = card.getTagAsNumber(`deck/${deckNameNeedle}`);
-        return deckTag !== undefined && deckTag >= 0;
-      }),
-
-      check(['set', 's'], setNameNeedle => {
-        const setTag = card.getTagAsString('set');
-        return setTag !== undefined && setTag.toLowerCase().startsWith(setNameNeedle);
-      }),
-
-      check(['tag'], tagNeedle => {
-        const [key, value] = tagNeedle.split('=').map(s => s.trim());
-        if (value) {
-          // If both key and value are specified (e.g. "tag:foo=bar"), match exact key and value on contains
-          return card.tags !== undefined && Object.entries(card.tags).some(([k, v]) =>
-            k.toLowerCase() === key && v !== undefined && v.toString().toLowerCase().includes(value)
-          );
-        } else {
-          // If only key is specified (e.g. "tag:foo"), match any tag with that key
-          return card.tags !== undefined && Object.keys(card.tags).some(k =>
-            k.toLowerCase() === key
-          );
-        }
-      }),
-    ];
-
-    return searchTerms.every(searchTerm => {
-      // First try all checks
-      for (const check of checks) {
-        const res = check(searchTerm);
-        if (res) return res.result;
-      }
-      // If the search term ends with ":", it's an incomplete key, so fail the search
-      if (searchTerm.endsWith(':')) return false;
-
-      // If no checks matched, default to name includes
-      return cardFace.name.toLowerCase().includes(searchTerm)
+        // If no checks matched, default to name includes
+        return cardFace.name.toLowerCase().includes(searchTerm)
+      });
     });
   });
 }
